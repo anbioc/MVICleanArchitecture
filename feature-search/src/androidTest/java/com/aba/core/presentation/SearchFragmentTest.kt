082da784cbe619d577e49ffbb553137217de76c2
mac <appcontactng@gmail.com>
2020-05-26 15:52:30 +0430

    Adding the Fragment Test Rule for Espresso to the Gradle dependecies.

--- feature-search/src/androidTest/java/com/aba/core/presentation/SearchFragmentTest.kt
+++ feature-search/src/androidTest/java/com/aba/core/presentation/SearchFragmentTest.kt
@@ -1,6 +1,13 @@
 package com.aba.core.presentation
 
+import androidx.test.rule.ActivityTestRule
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
 
+
+@RunWith(MockitoJUnitRunner::class)
 class SearchFragmentTest {
+
+
 
 }
